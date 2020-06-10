567c32bbcd2bae7d95ac8ee32d069844e44284aa
SNCollins20 <Joythe1st@aol.com>
2020-06-10 08:17:38 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let diceArray = [];
+let contaiuner = document.createElement('div');
+document.body.appendChild(container);
+
+class Dice { 
+    constructor (value){
+        this.value = value;
+        this.div = document.createElement('div');
+    }
+}
+let dice = [];
+
+
+
+
+
+function getRandomColor() {
+    let randomColor = color[Math.floor(Math.random() * color.length)];
+    return randomColor;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
